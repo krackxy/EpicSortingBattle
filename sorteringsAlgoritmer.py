9739dbd67d56325496e1ad87e046d02560891713
krackxy <kasper.oertoft.rasmussen@gmail.com>
2020-11-10 08:37:20 +0100

    there

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,32 +1,11 @@
 import random, math, sys
 
-def bogoSort(items):
-    # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
-    items = items.copy()
-    isSorted = None # Boolean til markering af, om listen er sorteret
-    attempts = 0 # Tællevariabel til at holde styr på antal af forsøg
-    while not isSorted:
-        attempts += 1
-        if attempts > len(items) * 5000: # Check for at stoppe tendensen mod uendeligt
-            print('Afbryder på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
-            items.sort()
-            return items
-        random.shuffle(items) # Bland alle elementer helt tilfældigt
-        isSorted = True # Vi går ud fra at listen tilfældigvis er sorteret,
-        # ...og prøver i denne løkke at bevise det modsatte
-        for index in range(len(items)-1):
-            if items[index] > items[index+1]:
-                isSorted = False
-                break # Bryd løkken hvis et eneste element er forkert sorteret
-    print('Sorteret efter {} forsøg'.format(attempts))
-    return items
 
+def InsertionSort(items):
+    for index in range(len(items)):
+        for j in range(index, len(items)):
+j = min
+            if items[j] < items[min]
+            j = min
 
-if __name__ == '__main__':
-    for i in range(10):
-        listen = list(range(1, 8))
-        sorteret = bogoSort(listen)
-        random.shuffle(listen)
-        print('Shuffled:\t', listen)
-        print('Sorted:\t\t', sorteret)
-        print('==============================================================')
+InsertionSort([6,3,8,9,4,7,4,6,0])
